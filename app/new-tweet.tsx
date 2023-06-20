8ad8b73681fccc771a9a4ba170838f4560606dd6
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-06-20 22:26:06 +0200

    add-floatingButton

---
+++ app/new-tweet.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function NewTweet() {
+    return (
+        <View>
+            <Text>Create new tweet</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({});

cf926ec68fc6f9d8a6479500c5107db5dcf955a3
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-06-26 19:46:58 +0200

    fix-bug-from-merge

--- app/new-tweet.tsx
+++ app/new-tweet.tsx
@@ -76,15 +76,3 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     }
 });
-=======
-import { StyleSheet, Text, View } from "react-native";
-
-export default function NewTweet() {
-    return (
-        <View>
-            <Text>Create new tweet</Text>
-        </View>
-    )
-}
-
-const styles = StyleSheet.create({});

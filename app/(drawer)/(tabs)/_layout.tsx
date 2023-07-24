7d43c0c930b55361ffad3ae0e8172f768f0ae236
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-07-24 18:58:30 +0200

    fetching tweets from api

--- app/(drawer)/(tabs)/_layout.tsx
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -1,14 +1,19 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs, useNavigation } from 'expo-router';
-import { Pressable, useColorScheme, Image } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs, useNavigation } from "expo-router";
+import { Pressable, useColorScheme, Image } from "react-native";
 
-import Colors from '../../../constants/Colors';
+import Colors from "../../../constants/Colors";
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: "two",
+};
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
@@ -17,15 +22,16 @@ function TabBarIcon(props: {
 function AvatarHeader() {
   const navigation = useNavigation();
   return (
-    <Pressable onPress={() => {navigation.openDrawer();}}>
-      <Image 
-        source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.png'}} 
-        style={{ width: 30, aspectRatio: 1, borderRadius: 30, marginLeft: 10}} 
+    <Pressable onPress={() => navigation.openDrawer()}>
+      <Image
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.png",
+        }}
+        style={{ width: 30, aspectRatio: 1, borderRadius: 40, marginLeft: 10 }}
       />
     </Pressable>
-
-  )
-};
+  );
+}
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -33,12 +39,13 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}>
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+      }}
+    >
       <Tabs.Screen
-        name="index"
+        name="feed"
         options={{
-          title: 'Tab One',
+          title: "Feed",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
@@ -47,20 +54,21 @@ export default function TabLayout() {
                   <FontAwesome
                     name="info-circle"
                     size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
+                    color={Colors[colorScheme ?? "light"].text}
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
                   />
                 )}
               </Pressable>
             </Link>
           ),
-          headerLeft: () => (<AvatarHeader />)
+          headerLeft: () => <AvatarHeader />,
         }}
       />
+
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
+          title: "Tab Two",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />

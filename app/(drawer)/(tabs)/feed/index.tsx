d247ed1616ffb4f93909229e2c170cd7b79b627e
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-07-25 18:23:55 +0200

    Setup React Query and fetch tweets

--- app/(drawer)/(tabs)/feed/index.tsx
+++ app/(drawer)/(tabs)/feed/index.tsx
@@ -1,41 +1,37 @@
-import { StyleSheet, View, FlatList, Pressable } from "react-native";
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  Pressable,
+  ActivityIndicator,
+  Text,
+} from 'react-native';
 
-import Tweet from "../../../../components/Tweet";
-import tweets from "../../../../assets/data/tweets";
-import { Entypo } from "@expo/vector-icons";
-import { Link } from "expo-router";
-import { useEffect, useState } from "react";
+import Tweet from '../../../../components/Tweet';
+import tweets from '../../../../assets/data/tweets';
+import { Entypo } from '@expo/vector-icons';
+import { Link } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { listTweets } from '../../../../lib/api/tweets';
+import { useQuery } from '@tanstack/react-query';
 
 export default function FeedScreen() {
-  const [tweets, setTweets] = useState([]);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['tweets'],
+    queryFn: listTweets,
+  });
 
-  useEffect(() => {
-    const fetchTweets = async () => {
-      const url = "http://localhost:3000/tweet";
-      const authToken =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbklkIjoxM30.rAixsNqKWh2bwhRuukO7rg2dBH8jb-x4-UpH6IuT9o4";
-      const res = await fetch(url, {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-      });
-      if (res.status !== 200) {
-        console.log("Error fetching tweets");
-      }
-      const data = await res.json();
-      console.log(data);
-      setTweets(data);
-    };
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
 
-    fetchTweets();
-  }, []);
+  if (error) {
+    return <Text>{error.message}</Text>;
+  }
 
   return (
     <View style={styles.page}>
-      <FlatList
-        data={tweets}
-        renderItem={({ item }) => <Tweet tweet={item} />}
-      />
+      <FlatList data={data} renderItem={({ item }) => <Tweet tweet={item} />} />
       <Link href="/new-tweet" asChild>
         <Entypo
           name="plus"
@@ -51,16 +47,16 @@ export default function FeedScreen() {
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: 'white',
   },
   floatingButton: {
-    backgroundColor: "#1C9BF0",
+    backgroundColor: '#1C9BF0',
     padding: 15,
     borderRadius: 25,
-    position: "absolute",
+    position: 'absolute',
     right: 15,
     bottom: 15,
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 2,
@@ -68,6 +64,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-    overflow: "hidden",
+    overflow: 'hidden',
   },
 });
---
+++ lib/api/tweets.ts
@@ -0,0 +1,17 @@
+import { API_URL, authToken } from "./config";
+export const listTweets = async () => {
+    const url = `${API_URL}/tweet`;
+    const res = await fetch(url, {
+        headers: {
+            Authorization: `Bearer ${authToken}`,
+        },
+    });
+    if (res.status === 401) {
+        throw new Error('Not authorized. Please sign in');
+    }
+    if (res.status !== 200) {
+        throw new Error('Error fetching tweets');
+    }
+    return await res.json();
+
+};

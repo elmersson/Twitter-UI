69812b8f75cfa88429ea7f3af6f5ea7b011f08f1
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-07-27 19:08:38 +0200

    fetch one tweet

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -5,75 +5,82 @@ import IconButton from './IconButton';
 import { Link } from 'expo-router';
 
 type TweetProps = {
-    tweet: TweetType;
-}
+  tweet: TweetType;
+};
 
 const Tweet = ({ tweet }: TweetProps) => {
-    return (
-      <Link href={`/tweet/${tweet.id}`} asChild>
-        <Pressable style={styles.container}>
-          <Image source={{uri: tweet.user.image}} style={styles.userImage}/>
-          <View style={styles.mainContainer}>
-                <View style={{flexDirection: 'row'}}>
-                <Text style={styles.name}>{tweet.user.name}</Text>
-                <Text style={styles.username}>{tweet.user.username} · 2h</Text>
-                <Entypo name="dots-three-horizontal" size={16} color="grey" style={{marginLeft: 'auto'}}/>
-            </View>
-            <Text style={styles.content}>{tweet.content}</Text>
-            {tweet.image && <Image source={{uri: tweet.image}} style={styles.image}/>}
-            <View style={styles.footer}>
-                <IconButton icon="comment" text={tweet.numberOfComments}/>
-                <IconButton icon="retweet" text={tweet.numberOfRetweets}/>
-                <IconButton icon="heart" text={tweet.numberOfLikes}/>
-                <IconButton icon="chart" text={tweet.impressions || 0}/>
-                <IconButton icon="share-apple"/>
-            </View>
+  return (
+    <Link href={`../app/(drawer)/(tabs)/feed/tweet/${tweet.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image source={{ uri: tweet.user.image }} style={styles.userImage} />
+        <View style={styles.mainContainer}>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={styles.name}>{tweet.user.name}</Text>
+            <Text style={styles.username}>{tweet.user.username} · 2h</Text>
+            <Entypo
+              name="dots-three-horizontal"
+              size={16}
+              color="grey"
+              style={{ marginLeft: 'auto' }}
+            />
           </View>
-        </Pressable>
-      </Link>
-    );
-}
+          <Text style={styles.content}>{tweet.content}</Text>
+          {tweet.image && (
+            <Image source={{ uri: tweet.image }} style={styles.image} />
+          )}
+          <View style={styles.footer}>
+            <IconButton icon="comment" text={tweet.numberOfComments} />
+            <IconButton icon="retweet" text={tweet.numberOfRetweets} />
+            <IconButton icon="heart" text={tweet.numberOfLikes} />
+            <IconButton icon="chart" text={tweet.impressions || 0} />
+            <IconButton icon="share-apple" />
+          </View>
+        </View>
+      </Pressable>
+    </Link>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flexDirection: 'row',
-      padding: 10,
-      borderBottomWidth: StyleSheet.hairlineWidth,
-      borderColor: 'lightgrey',
-      backgroundColor: 'white'
-    },
-    mainContainer: {
-      marginLeft: 10,
-      flex: 1,
-    },
-    userImage: {
-      width: 50,
-      height: 50,
-      borderRadius: 50,
-    },
-    name: {
-      fontWeight: '600',
-    },
-    username: {
-        color: 'grey',
-        marginLeft: 5,
-        fontWeight: '400',
-      },
-    content: {
-      lineHeight: 20,
-      marginTop: 5,
-    },
-    image: {
-        width: '100%',
-        aspectRatio: 16 / 9,
-        marginTop: 10,
-        borderRadius: 15,
-    },
-    footer: {
-        flexDirection: 'row',
-        marginVertical: 5,
-        justifyContent: 'space-between'
-    },
-  });
+  container: {
+    flexDirection: 'row',
+    padding: 10,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: 'lightgrey',
+    backgroundColor: 'white',
+  },
+  mainContainer: {
+    marginLeft: 10,
+    flex: 1,
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  name: {
+    fontWeight: '600',
+  },
+  username: {
+    color: 'grey',
+    marginLeft: 5,
+    fontWeight: '400',
+  },
+  content: {
+    lineHeight: 20,
+    marginTop: 5,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 16 / 9,
+    marginTop: 10,
+    borderRadius: 15,
+  },
+  footer: {
+    flexDirection: 'row',
+    marginVertical: 5,
+    justifyContent: 'space-between',
+  },
+});
 
-  export default Tweet;
+export default Tweet;
--- lib/api/tweets.ts
+++ lib/api/tweets.ts
@@ -13,5 +13,20 @@ export const listTweets = async () => {
         throw new Error('Error fetching tweets');
     }
     return await res.json();
-
 };
+
+export const getTweet = async (id: string) => {
+    const url = `${API_URL}/tweet/${id}`;
+    const res = await fetch(url, {
+        headers: {
+            Authorization: `Bearer ${authToken}`,
+        },
+    });
+    if (res.status === 401) {
+        throw new Error('Not authorized. Please sign in');
+    }
+    if (res.status !== 200) {
+        throw new Error('Error fetching tweets');
+    }
+    return await res.json();
+}
